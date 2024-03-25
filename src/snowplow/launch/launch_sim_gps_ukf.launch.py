7a99b6dca8b8c002d84c78eb17b1b8728cceba4b
aduno <aduno7647@gmail.com>
2024-03-25 23:37:16 +0000

    Cleaning up configs, modifying launch files, and adding slam output

--- src/snowplow/launch/launch_sim_gps_ukf.launch.py
+++ src/snowplow/launch/launch_sim_gps_ukf.launch.py
@@ -20,7 +20,7 @@ def generate_launch_description():
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name='snowplow' #<--- CHANGE ME
+    package_name='snowplow' 
 
     
     rl_params_file = os.path.join(get_package_share_directory(package_name), 
