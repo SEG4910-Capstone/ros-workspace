83456e36db061ee0cff1bebff85a850d990487c9
Aduno <sanp100d@gmail.com>
2024-03-30 23:29:26 +0000

    Fixing up config, and adding localization

--- README.md
+++ README.md
@@ -9,3 +9,23 @@
 
 To not do this each time you open up a new terminal goto the 
 ```nano ~/.bashrc```, and add ```source {full path to your setup.bash file}```. Ex. ```source /home/ros-workspace/install/setup.bash```
+
+## Mapping
+To launch the simulation there are 3 different launch commands
+```ros2 launch snowplow launch_sim_gps.launch.py```
+This command launches the virtual environment with the simulated physical robot and sensors. </br>
+```ros2 launch snowplow slam.launch.py use_sim_time:=true params_file:=[location to mapping slam config]``` 
+This command launches the SLAM toolbox in mapping mode where you can save the map after you navigate around the area. The params_file parameter is optional. </br>
+```ros2 launch snowplow navigation_launch.py use_sim_time:=true```
+This command launches the nav2 stack which has all the plugins for waypoint following, behavior tree, path planning, etc.
+
+## Localization
+After generating a map of the surrounding, AMCL can be used to localize the robot within a given area. For this
+there are 3 launch commands to run.
+```ros2 launch snowplow launch_sim_gps.launch.py```
+This command launches the virtual environment with the simulated physical robot and sensors. </br>
+```ros2 launch snowplow localization.launch.py use_sim_time:=true map:=[the yaml file generated from SLAM mapping] params_file:=[location to nav2 config]``` 
+This command launches the map server and AMCL. The params_file parameter is optional again as it is defaulted to a
+preset config file. </br>
+```ros2 launch snowplow navigation_launch.py use_sim_time:=true map_subscribe_transient_local:=true```
+This command launches the nav2 stack which has all the plugins for waypoint following, behavior tree, path planning, etc.
--- src/snowplow/launch/launch_sim_gps.launch.py
+++ src/snowplow/launch/launch_sim_gps.launch.py
@@ -95,15 +95,16 @@ def generate_launch_description():
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
-    # gazebo = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-    #                 launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-    #          )
-    # The above method of launching gazebo causes some issues that I'm not sure how to resolve. For now, this will work.
-    gazebo = ExecuteProcess(
-            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world], output='screen'
-            )
+    gazebo_srv = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'),'launch','gzserver.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true', 'world': world, 'params_file': gazebo_params_file}.items()
+    )
+    gazebo_client = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'),'launch','gzclient.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -156,7 +157,9 @@ def generate_launch_description():
         rsp,
         joystick,
         twist_mux,
-        gazebo,
+        # gazebo,
+        gazebo_srv,
+        gazebo_client,
         spawn_entity,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
