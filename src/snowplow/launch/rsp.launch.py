7ae03d82edac993bdf29bf49b78f17630064c745
Olivier Caron <ocaro009@uottawa.ca>
2024-10-03 21:28:09 -0400

    Updating ros-workspace to work with arm64 jetson devices on ubuntu 22.04

--- src/snowplow/launch/rsp.launch.py
+++ src/snowplow/launch/rsp.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration, Command
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch_ros.actions import Node
 
 import xacro
@@ -19,9 +19,12 @@ def generate_launch_description():
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('snowplow'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    
+    map_file = os.path.join(pkg_path, 'worlds', 'test.sdf')
+
     robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
+    robot_description = os.path.join(pkg_path, 'description', 'temp_robot_urdf.urdf')
+
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
@@ -31,6 +34,25 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    gazebo = ExecuteProcess(
+        cmd=['ign', 'gazebo', '-r', '-v', '4', map_file],
+        output='screen'
+    )
+
+    # spawn_entity = Node(
+    #     package='ignition_ros',
+    #     executable='spawn_entity',
+    #     name='spawn_robot',
+    #     arguments=[
+    #         '-name', 'snowplow',
+    #         '-file', robot_description,
+    #         '-x', '0','-y', '0','-z', '1'
+    #     ],
+    #     output="screen"
+    # )
+    spawn = Node( package='ros_gz_sim', executable='create', 
+                arguments=[ '-name', 'snowplow', '-topic', 'robot_description', ], 
+                output='screen') 
 
     # Launch!
     return LaunchDescription([
@@ -42,6 +64,13 @@ def generate_launch_description():
             'use_ros2_control',
             default_value='true',
             description='Use ros2_control if true'),
-
-        node_robot_state_publisher
+        # ExecuteProcess(
+        # cmd=['ro2','run','xacro','xacro', xacro_file, 'use_ros2_control:=' + use_ros2_control,
+        #     'sim_mode:=' + use_sim_time, '-o', robot_description],
+        # output='screen',
+        # shell=True
+        # ),   
+        node_robot_state_publisher,
+        gazebo,
+        spawn
     ])
