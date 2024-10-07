4e38c17fb919ea998d434dff7f59549ce8336809
aduno <aduno7647@gmail.com>
2024-10-07 19:29:54 +0000

    Updating code to work with gazebo fortress humble

---
+++ src/snowplow/launch/launch_sim_ign.launch.py
@@ -0,0 +1,149 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart, OnProcessExit
+
+from launch_ros.actions import Node
+
+import xacro
+
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='snowplow' #<--- CHANGE ME
+    package_directory = get_package_share_directory(package_name)
+    
+    world = os.path.join(package_directory, 
+                         "worlds", "capstone_1.world") #<--- Change map as required
+    map_file = os.path.join(package_directory, 'worlds', 'test.sdf')
+
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    package_directory,'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    twist_mux_params = os.path.join(package_directory,'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
+    
+
+    xacro_file = os.path.join(package_directory,
+                              'description',
+                              'robot.urdf_ign.xacro')    
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    params = {'robot_description': doc.toxml(), 'use_sim_time': True, 'use_ros2_control:=': True, 'sim_mode:=': True}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params],
+    )
+
+    ignition_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=['-string', doc.toxml(),
+                   '-name', 'snowplow',
+                   '-allow_renaming', 'true'],
+    )
+    
+    # Gazebo 
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock'],
+        output='screen'
+    )
+    gazebo = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(get_package_share_directory('ros_ign_gazebo'),
+                              'launch', 'ign_gazebo.launch.py')]),
+            launch_arguments=[('gz_args', [' -r -v 4 empty.sdf'])])
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    # load_diff_drive_controller = ExecuteProcess(
+    #     cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+    #          'diff_cont'],
+    #     output='screen'
+    # )
+
+    # New method of spawning the controllers
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
+    controller_params_file = os.path.join(package_directory,'config','my_controllers.yaml')
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description},
+                    controller_params_file]
+    )
+
+    delayed_controller_manager = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=ignition_spawn_entity,
+            on_exit=controller_manager
+        )
+    )
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_broad_spawner,
+            on_exit=[diff_drive_spawner],
+        )
+    )
+
+
+
+    # Launch them all!
+    return LaunchDescription([
+        bridge,
+        gazebo,
+        delayed_controller_manager,
+        delayed_joint_broad_spawner,
+        delayed_diff_drive_spawner,
+        node_robot_state_publisher,
+        ignition_spawn_entity,
+        joystick,
+        twist_mux
+    ])
