fceb4e5dc789102731397717d8295a83bd1bc233
Aduno <sanp100d@gmail.com>
2024-12-02 06:56:15 +0000

    Renaming slam localization file (redundant and confusing)

--- src/snowplow/launch/physical_snowplow_bringup.launch.py
+++ src/snowplow/launch/physical_snowplow_bringup.launch.py
@@ -23,7 +23,7 @@ def generate_launch_description():
     )
 
     localization = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/physical_slam_localization.launch.py'))
+        PythonLaunchDescriptionSource(os.path.join(pkg_share, 'launch/physical_localization.launch.py'))
     )
 
     navigation = IncludeLaunchDescription(
